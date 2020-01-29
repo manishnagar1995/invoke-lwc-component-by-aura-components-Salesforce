7bc1040013884bdaed48d64f6adc4aabdad15637
manish nagar <33551293+manishnagar1995@users.noreply.github.com>
2020-01-29 18:16:19 +0530

    Final done create record page new contact

--- invoke lwc component in quick action/force-app/main/default/lwc/lWC_invoke_By_Quick_Action/lWC_invoke_By_Quick_Action.js
+++ invoke lwc component in quick action/force-app/main/default/lwc/lWC_invoke_By_Quick_Action/lWC_invoke_By_Quick_Action.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+/* eslint-disable no-console */
+import { LightningElement,api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class LWC_invoke_By_Quick_Action extends LightningElement {}
+export default class LWC_invoke_By_Quick_Action extends NavigationMixin(LightningElement) {
+
+    @api recordId;
+    connectedCallback() {
+        this.navigateToNewRecordPage();
+        console.log('test======>',this.recordId);
+    }
+    navigateToNewRecordPage() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+               // "recordId":"0010o00002cNasdAAC",
+                "objectApiName": "Contact",
+                "actionName": "new"
+             },
+            state : {
+               // nooverride: '1',
+                defaultFieldValues : "LastName = manish,FirstName= nagar,"
+            }
+        });
+    }
+
+}
